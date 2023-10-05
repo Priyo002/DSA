a0d3613258bb4d7092340f5ada6d64bcbe6cab66
Priyabrata Das <priyobapi@gmail.com>
2023-10-05 18:13:14 +0530

    Solving CSES

---
+++ CSES/Sorting And Searching/Collecting_Numbers1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb emplace_back
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define endl '\n'
+#define sort(X) sort(X.begin(),X.end())
+const int mod = 1e9 + 7;
+const long long INF = 1e18;
+
+void init() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> seen(n + 1, 0);
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		if (seen[a - 1] == 1) {
+			seen[a] = 1;
+		}
+		else {
+			seen[a] = 1;
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
+
+int32_t main() {
+
+	init();
+	//clock_t time_req;
+	//time_req = clock();
+
+
+	solve();
+
+	//time_req = clock() - time_req;
+	//cout << endl << "Time Taken is ";
+	//cout << (float)time_req / CLOCKS_PER_SEC << " Seconds" << endl;
+
+}
