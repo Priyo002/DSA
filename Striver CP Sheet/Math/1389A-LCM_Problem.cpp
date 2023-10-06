4f5ffe91bbc72502d11db58c153ae5f0e4ec4381
Priyabrata Das <priyobapi@gmail.com>
2023-10-06 19:20:20 +0530

    Solving Striver CP sheet

---
+++ Striver CP Sheet/Math/1389A-LCM_Problem.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+
+#define int long long
+#define pb emplace_back
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define endl '\n'
+#define sort(X) sort(X.begin(),X.end())
+const int mod = 1e9 + 7;
+const long long INF = 1e18;
+
+void init() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+}
+
+void solve() {
+	int l, r;
+	cin >> l >> r;
+	int x = 2 * l;
+	if (x > r) cout << -1 << " " << -1;
+	else cout << l << " " << x;
+	cout << endl;
+}
+
+int32_t main() {
+
+	init();
+	//clock_t time_req;
+	//time_req = clock();
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+	//time_req = clock() - time_req;
+	//cout << endl << "Time Taken is ";
+	//cout << (float)time_req / CLOCKS_PER_SEC << " Seconds" << endl;
+
+}
