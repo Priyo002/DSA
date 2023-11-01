78c2972b43fba2154dea0c20441b0b93f94b9a15
Priyabrata Das <priyobapi@gmail.com>
2023-11-01 21:13:46 +0530

    Participated in Codechef Contest

---
+++ Contests/Codechef/Starters-106/q1.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb emplace_back
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define endl '\n'
+#define sort(X) sort(X.begin(),X.end())
+const int mod = 1e9 + 7;
+const long long INF = 1e18;
+
+void init() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("inputf.in", "r", stdin);
+	freopen("outputf.in", "w", stdout);
+#endif
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << n*(n - 1) << endl;
+}
+
+int32_t main() {
+
+	init();
+	//clock_t time_req;
+	//time_req = clock();
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+	//time_req = clock() - time_req;
+	//cout << endl << "Time Taken is ";
+	//cout << (float)time_req / CLOCKS_PER_SEC << " Seconds" << endl;
+
+}
