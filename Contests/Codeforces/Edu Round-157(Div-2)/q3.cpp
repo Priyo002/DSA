ed375cffbac71c70a91304b45f5344be91cfd9f3
Priyabrata Das <priyobapi@gmail.com>
2023-11-04 20:44:13 +0530

    Upsolved

--- Contests/Codeforces/Edu Round-157(Div-2)/q3.cpp
+++ Contests/Codeforces/Edu Round-157(Div-2)/q3.cpp
@@ -23,115 +23,51 @@ void init() {
 
 int sum(string& str) {
 	int s = 0;
-	for (int i = 0; i < str.size(); i++) {
-		s += (str[i] - '0');
+	for (auto ch : str) {
+		s += (ch - '0');
 	}
 	return s;
 }
 
-bool check(string& str, int l, int total) {
-	int s1 = 0;
-	for (int i = 0; i < l / 2; i++) {
-		s1 += (str[i] - '0');
-	}
-	if (s1 * 2 == total) return true;
-	return false;
-}
-
 void solve() {
-	int n;
+	int n, ans = 0;
 	cin >> n;
-	vector<string> arr(n);
-	int ans = 0;
-	for (int i = 0; i < n; i++) cin >> arr[i];
+	vector<string> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
 
-	vector<vector<int>> v(1000);
-	vector<int> e, o;
-	for (int i = 0; i < n; i++) {
-		int k = arr[i].size();
-		if (k & 1) o.push_back(i);
-		else e.push_back(i);
-		v[k].push_back(i);
-
+	vector<map<int, int>> mp(6);
+	vector<pair<int, string>> vs;
+	for (auto e : v) {
+		vs.push_back({e.size(), e});
 	}
-	set<pair<int, int>> st;
-	for (auto it : o) {
-		string temp1 = arr[it], temp2 = arr[it];
-		for (int i = 1; i <= 5; i += 2) {
+	sort(vs);
+	v.clear();
+	for (auto e : vs) {
+		v.push_back(e.second);
+	}
+	for (auto s : v) {
+		int tot = 0;
 
-			for (auto j : v[i]) {
-				string t = arr[j];
-				temp1 += t;
-				temp2 = t + temp2;
-
-				int s = sum(temp1);
-
-				if (check(temp1, temp1.size(), s)) {
-					st.insert({it, j});
-					ans += 1;
-				}
-
-				s = sum(temp2);
-
-				if (check(temp2, temp2.size(), s)) {
-					if (st.count({it, j})) {
-						auto m = st.find({it, j});
-						if (m->first != m->second) {
-							st.insert({it, j});
-							ans += 1;
-						}
-						else ans++;
-					}
-					else {
-						st.insert({it, j});
-						ans++;
-					}
-				}
-
-			}
+		for (auto d : s) {
+			tot += (d - '0');
 		}
-	}
-	for (auto it : e) {
-		string temp1 = arr[it], temp2 = arr[it];
-		for (int i = 2; i <= 4; i += 2) {
-
-			for (auto j : v[i]) {
-				string t = arr[j];
-				temp1 += t;
-				temp2 = t + temp2;
-
-				int s = sum(temp1);
-
-				if (check(temp1, temp1.size(), s)) {
-					st.insert({it, j});
-					ans += 1;
-				}
-
-				s = sum(temp2);
-
-				if (check(temp2, temp2.size(), s)) {
-					if (st.find({it, j}) != st.end()) {
-						auto m = st.find({it, j});
-						if (m->first != m->second) {
-							st.insert({it, j});
-							ans += 1;
-						}
-						else ans++;
-					}
-					else {
-						st.insert({it, j});
-						ans++;
-					}
-				}
-
+		int a = 0, b = 0;
+		for (int len = s.size() + 2 - s.size() % 2; len <= 10; len += 2) {
+			if (len > 2 * s.size()) break;
+			a = 0;
+			b = 0;
+			for (int i = 0; i < s.size() - len / 2; i++) {
+				a += s[i] - '0';
+				b += s[s.size() - i - 1] - '0';
 			}
+
+			ans += mp[len - s.size()][tot - 2 * a];
+			ans += mp[len - s.size()][tot - 2 * b];
 		}
+		mp[s.size()][tot]++;
+		ans++;
 	}
-	for (auto x : st) cout << x.first << "," << x.second << " ";
-	cout << endl;
-	cout << st.size();
-	cout << endl;
-	cout << ans;
+	cout << ans << endl;
 }
 
 int32_t main() {
